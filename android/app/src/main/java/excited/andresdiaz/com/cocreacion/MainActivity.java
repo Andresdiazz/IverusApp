cd177c87bed1f3153ad8041416bfd94416dac80d
amitkumar <expertamitch@gmail.com>
2019-09-05 19:30:31 +0530

    Facebook Fix Push code test

--- android/app/src/main/java/excited/andresdiaz/com/cocreacion/MainActivity.java
+++ android/app/src/main/java/excited/andresdiaz/com/cocreacion/MainActivity.java
@@ -1,6 +1,9 @@
 package excited.andresdiaz.com.cocreacion;
 
 import android.os.Bundle;
+
+import com.facebook.FacebookSdk;
+
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
@@ -9,5 +12,5 @@ public class MainActivity extends FlutterActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
-  }
+   }
 }
