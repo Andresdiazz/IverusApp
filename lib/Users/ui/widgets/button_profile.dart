07a1e30d1ea7374b08eb33b03113ae4d2406ed57
amitkumar <expertamitch@gmail.com>
2019-09-06 06:49:33 +0530

    Profile Image Progress Bar

--- lib/Users/ui/widgets/button_profile.dart
+++ lib/Users/ui/widgets/button_profile.dart
@@ -1,13 +1,11 @@
-
 import 'package:cocreacion/Users/bloc/bloc_user.dart';
 import 'package:cocreacion/Users/model/user.dart';
 import 'package:cocreacion/Users/ui/screens/profile_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:generic_bloc_provider/generic_bloc_provider.dart';
-
+import 'package:percent_indicator/circular_percent_indicator.dart';
 
 class ButtonProfile extends StatelessWidget {
-
   double fontSize;
   double width;
   double height;
@@ -16,26 +14,24 @@ class ButtonProfile extends StatelessWidget {
 
   UserBloc userBloc;
 
-  ButtonProfile({@required this.userBloc, this.user, this.fontSize, this.height, this.width});
+  ButtonProfile(
+      {@required this.userBloc,
+      this.user,
+      this.fontSize,
+      this.height,
+      this.width});
 
   @override
   Widget build(BuildContext context) {
-
-      userBloc = BlocProvider.of<UserBloc>(context);
-
-      return StreamBuilder(
-        stream: userBloc.streamFirebase,
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-
-
-
-          if ( !snapshot.hasData  || snapshot.hasError){
-             return profile(context);
-          } else {
-            return   showProfileData(snapshot, context);
-
-          }
-
+    userBloc = BlocProvider.of<UserBloc>(context);
+    return StreamBuilder(
+      stream: userBloc.streamFirebase,
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (!snapshot.hasData || snapshot.hasError) {
+          return profile(context);
+        } else {
+          return showProfileData(snapshot, context);
+        }
 
         /*  switch (snapshot.connectionState) {
             case ConnectionState.active:
@@ -54,40 +50,33 @@ class ButtonProfile extends StatelessWidget {
             default:
               return showProfileData(snapshot, context);
           }*/
-        },
-      );
+      },
+    );
+  }
 
-    }
-
-  Widget profile(BuildContext context){
+  Widget profile(BuildContext context) {
     final photo = Container(
-      margin: EdgeInsets.only(
-          top: 30.0,
-          right: 10.0
-      ),
+      margin: EdgeInsets.only(top: 30.0, right: 10.0),
 
       width: width,
       //40.0,
       height: height,
-    //40.0,
+      //40.0,
 
       decoration: BoxDecoration(
           shape: BoxShape.circle,
           image: DecorationImage(
-              fit: BoxFit.cover,
-              image: AssetImage("assets/img/profile.jpg")
-          )
-      ),
+              fit: BoxFit.cover, image: AssetImage("assets/img/profile.jpg"))),
     );
 
     return InkWell(
-      onTap: (){
-        Navigator.push(context, MaterialPageRoute(builder: (context)=> ProfileScreen()));
+      onTap: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => ProfileScreen()));
         return BlocProvider<UserBloc>(
           bloc: UserBloc(),
           child: ProfileScreen(),
         );
-
       },
       child: photo,
     );
@@ -97,67 +86,76 @@ class ButtonProfile extends StatelessWidget {
     user = User(
         name: snapshot.data.displayName,
         photoURL: snapshot.data.photoUrl,
-        email: snapshot.data.email
-    );
+        email: snapshot.data.email);
 
     final userName = Container(
-      margin: EdgeInsets.only(
-          left: 40.0,
-          top:10.0
-      ),
-
-      child: Center(
-        child: Text(
-          user.name,
-          //"Andres Diaz",
-          textAlign: TextAlign.end,
-          style: TextStyle(
-              fontFamily: "Lato",
-              fontSize: fontSize,
-              fontWeight: FontWeight.w900,
-              color: Colors.white
+        margin: EdgeInsets.only(left: 40.0, top: 10.0),
+        child: Center(
+          child: Text(
+            user.name,
+            //"Andres Diaz",
+            textAlign: TextAlign.end,
+            style: TextStyle(
+                fontFamily: "Lato",
+                fontSize: fontSize,
+                fontWeight: FontWeight.w900,
+                color: Colors.white),
           ),
-        ),
-      )
-    );
+        ));
 
     final photo = Container(
       margin: EdgeInsets.only(top: 20),
-
-
       width: width,
       height: height,
-
       decoration: BoxDecoration(
           shape: BoxShape.circle,
           image: DecorationImage(
               fit: BoxFit.cover,
               image: //AssetImage("assets/img/profile.jpg")
-              NetworkImage(user.photoURL)
-          )
+                  NetworkImage(user.photoURL))),
+    );
+
+    var updatedPhoto = Container(
+      margin: const EdgeInsets.only(top: 20),
+      height: height,
+      width: width,
+      child: CircularPercentIndicator(
+        radius: width / 1.2,
+        lineWidth: width / 20,
+        percent: getPercent(121),
+        progressColor: Colors.black,
+        center: Container(
+           width: width/1.3,
+          height: width/1.3,
+          decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: //AssetImage("assets/img/profile.jpg")
+                      NetworkImage(user.photoURL))),
+        ),
       ),
     );
 
     return Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          photo,
-          Center(
-            child: userName,
-          )
-        ],
-      );
-
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        updatedPhoto,
+        Center(
+          child: userName,
+        )
+      ],
+    );
   }
 
+  getPercent(int points) {
+    if (points <= 100) {
+      return 0.33;
+    } else if (points > 100 && points <= 200) {
+      return 0.66;
+    } else if (points > 200 && points <= 300) {
+      return 1.0;
+    }
   }
-
-
-
-
-
-
-
-
-
+}
