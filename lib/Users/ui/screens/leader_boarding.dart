c94213b554f8bb3609d044aeb0d838fe1bed2d48
amitkumar <expertamitch@gmail.com>
2019-09-09 21:20:57 +0530

    Modified Leaderboard

--- lib/Users/ui/screens/leader_boarding.dart
+++ lib/Users/ui/screens/leader_boarding.dart
@@ -72,131 +72,139 @@ class _LeaderBoardingState extends State<LeaderBoarding> {
       stream: homeBloc.leaderBoard,
       builder: (BuildContext context, AsyncSnapshot<List<User>> snapshot) {
         return Container(
-          height: MediaQuery
-              .of(context)
-              .size
-              .height * 0.6,
-          child: SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                Center(
-                  child: Container(
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: <Widget>[lives, video, share],
-                    ),
+          height: MediaQuery.of(context).size.height * 0.6,
+          child: Column(
+            children: <Widget>[
+              Center(
+                child: Container(
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[lives, video, share],
                   ),
                 ),
-                SizedBox(height: 30),
-
-                //            getPopulatedList(snapshot.data),
-
-                Container(
-                  //margin: EdgeInsets.only(top: 300),
-//                  width: 350,
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                        colors: [
-                          Colors.white,
-                          Colors.white,
-                        ],
-                        stops: [
-                          0.4,
-                          0.30
-                        ],
-                        begin: Alignment.topLeft,
-                        end: Alignment.bottomLeft,
-                        tileMode: TileMode.repeated),
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: const [
-                      BoxShadow(color: Colors.black, blurRadius: 20)
-                    ],
-                  ),
-                  child: Center(
-                    child: DataTable(columns: <DataColumn>[
-                      DataColumn(
-                        label: Text(
-                          "Image",
-                          style: TextStyle(
-                              fontSize: 15,
-                              color: Colors.indigoAccent,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                      DataColumn(
-                        label: Text(
-                          "Name",
-                          style: TextStyle(
-                              fontSize: 15,
-                              color: Colors.indigoAccent,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                      DataColumn(
-                          label: Text("Points",
-                              style: TextStyle(
-                                  fontSize: 15,
-                                  color: Colors.indigoAccent,
-                                  fontWeight: FontWeight.bold))),
-                      DataColumn(
-                          label: Text("Gift",
-                              style: TextStyle(
-                                  fontSize: 15,
-                                  color: Colors.indigoAccent,
-                                  fontWeight: FontWeight.bold)))
-                    ], rows:
-                    List.generate(snapshot.data.length, (index) =>
-                        getDataRow(snapshot.data[index]))
-                      /*    DataRow(
-                        cells: <DataCell>[
-                           DataCell(Text("Andres Diaz")),
-                          DataCell(CellText(
-                            color: Colors.amber,
-                            text: "300",
-                            icon: Icons.local_parking,
-                          )),
-                          DataCell(CellText(
-                            color: Colors.lightGreen,
-                            text: "1000",
-                            icon: Icons.monetization_on,
-                          ))
-                        ],
-                      ),
-                      DataRow(
-                        cells: <DataCell>[
-                          DataCell(Text("John")),
-                          DataCell(CellText(
-                            color: Colors.amber,
-                            text: "210",
-                            icon: Icons.local_parking,
-                          )),
-                          DataCell(CellText(
-                            color: Colors.lightGreen,
-                            text: "500",
-                            icon: Icons.monetization_on,
-                          ))
-                        ],
-                      ),
-                      DataRow(
-                        cells: <DataCell>[
-                          DataCell(Text("Maurico")),
-                          DataCell(CellText(
-                            color: Colors.amber,
-                            text: "100",
-                            icon: Icons.local_parking,
-                          )),
-                          DataCell(CellText(
-                            color: Colors.lightGreen,
-                            text: "200",
-                            icon: Icons.monetization_on,
-                          ))
-                        ],
-                      )*/),
-                  ),
+              ),
+              SizedBox(height: 30),
+//                Padding(
+//                  padding: const EdgeInsets.all(12.0),
+//                  child: Row(
+//                    children: <Widget>[
+//                      SizedBox(
+//                        width: MediaQuery.of(context).size.width * 0.22,
+//                        child: Align(
+//                          child: Text(
+//                            "Image",
+//                            style: TextStyle(
+//                                fontSize: 15,
+//                                color: Colors.indigoAccent,
+//                                fontWeight: FontWeight.bold),
+//                          ),
+//                          alignment: Alignment(-1, 0),
+//                        ),
+//                      ),
+//                      SizedBox(
+//                        width: MediaQuery.of(context).size.width * 0.3,
+//                        child: Align(
+//                            child: Text(
+//                              "Name",
+//                              style: TextStyle(
+//                                  fontSize: 15,
+//                                  color: Colors.indigoAccent,
+//                                  fontWeight: FontWeight.bold),
+//                            ),
+//                            alignment: Alignment(-1, 0)),
+//                      ),
+//                      SizedBox(
+//                        width: MediaQuery.of(context).size.width * 0.22,
+//                        child: Align(
+//                            child: Text(
+//                              "Points",
+//                              style: TextStyle(
+//                                  fontSize: 15,
+//                                  color: Colors.indigoAccent,
+//                                  fontWeight: FontWeight.bold),
+//                            ),
+//                            alignment: Alignment(-1, 0)),
+//                      ),
+//                      Align(
+//                          child: Text(
+//                            "Points",
+//                            style: TextStyle(
+//                                fontSize: 15,
+//                                color: Colors.indigoAccent,
+//                                fontWeight: FontWeight.bold),
+//                          ),
+//                          alignment: Alignment(-1, 0)),
+//                    ],
+//                  ),
+//                ),
+              SingleChildScrollView(
+                child: Container(
+                  height: MediaQuery.of(context).size.height * 0.5,
+                  child: getPopulatedList(snapshot.data),
                 ),
-              ],
-            ),
+              )
+//                Container(
+//
+//                  margin: EdgeInsets.only(left: 8,right: 8),
+////                  width: 350,
+//                  decoration: BoxDecoration(
+//                    gradient: LinearGradient(
+//                        colors: [
+//                          Colors.white,
+//                          Colors.white,
+//                        ],
+//                        stops: [
+//                          0.4,
+//                          0.30
+//                        ],
+//                        begin: Alignment.topLeft,
+//                        end: Alignment.bottomLeft,
+//                        tileMode: TileMode.repeated),
+//                    borderRadius: BorderRadius.circular(20),
+//                    boxShadow: const [
+//                      BoxShadow(color: Colors.black, blurRadius: 20)
+//                    ],
+//                  ),
+//                  child: Center(
+//                    child: DataTable(
+//                        columns: <DataColumn>[
+//                          DataColumn(
+//                            label: Text(
+//                              "Image",
+//                              style: TextStyle(
+//                                  fontSize: 15,
+//                                  color: Colors.indigoAccent,
+//                                  fontWeight: FontWeight.bold),
+//                            ),
+//                          ),
+//                          DataColumn(
+//                            label: Text(
+//                              "Name",
+//                              style: TextStyle(
+//                                  fontSize: 15,
+//                                  color: Colors.indigoAccent,
+//                                  fontWeight: FontWeight.bold),
+//                            ),
+//                          ),
+//                          DataColumn(
+//                              label: Text("Points",
+//                                  style: TextStyle(
+//                                      fontSize: 15,
+//                                      color: Colors.indigoAccent,
+//                                      fontWeight: FontWeight.bold))),
+//                          DataColumn(
+//                              label: Text("Gift",
+//                                  style: TextStyle(
+//                                      fontSize: 15,
+//                                      color: Colors.indigoAccent,
+//                                      fontWeight: FontWeight.bold)))
+//                        ],
+//                        rows: List.generate(snapshot.data.length,
+//                            (index) => getDataRow(snapshot.data[index]))),
+//                  ),
+//                ),
+            ],
           ),
         );
       },
@@ -206,9 +214,14 @@ class _LeaderBoardingState extends State<LeaderBoarding> {
   getDataRow(User user) {
     return DataRow(
       cells: <DataCell>[
-        DataCell(CircleAvatar(
-          maxRadius: 27,
-          backgroundImage: NetworkImage(user.photoURL),
+        DataCell(Card(
+          child: CircleAvatar(
+            radius: 20,
+            backgroundImage: NetworkImage(user.photoURL),
+          ),
+          elevation: 2,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
         )),
         DataCell(Text(user.name)),
         DataCell(CellText(
@@ -218,7 +231,7 @@ class _LeaderBoardingState extends State<LeaderBoarding> {
         )),
         DataCell(CellText(
           color: Colors.lightGreen,
-          text: user.gift!=null? user.gift:'--',
+          text: user.gift != null ? user.gift : '--',
           icon: Icons.monetization_on,
         ))
       ],
@@ -226,48 +239,54 @@ class _LeaderBoardingState extends State<LeaderBoarding> {
   }
 
   getPopulatedList(List<User> leaderboard) {
-    ListView.builder(
-        padding: EdgeInsets.only(bottom: 10),
+    return ListView.builder(
+//        physics: const NeverScrollableScrollPhysics(),
+        shrinkWrap: true,
+        scrollDirection: Axis.vertical,
+        padding: EdgeInsets.all(12),
         itemCount: leaderboard.length,
         itemBuilder: (context, i) {
 //          var values = snapshot.data[i];
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: <Widget>[
-                SizedBox(
-                  width: MediaQuery
-                      .of(context)
-                      .size
-                      .width * 0.25,
-                  child: Align(
-                    child: CircleAvatar(
-                      maxRadius: 27,
-                      backgroundImage: NetworkImage(leaderboard[i].photoURL),
+          return Card(
+            child: Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: Row(
+                children: <Widget>[
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.22,
+                    child: Align(
+                      child: CircleAvatar(
+                        maxRadius: 20,
+                        backgroundImage: NetworkImage(leaderboard[i].photoURL),
+                      ),
+                      alignment: Alignment(-1, 0),
                     ),
-                    alignment: Alignment(-1, 0),
                   ),
-                ),
-                SizedBox(
-                  width: MediaQuery
-                      .of(context)
-                      .size
-                      .width * 0.3,
-                  child: Align(
-                      child: Text(leaderboard[i].name),
-                      alignment: Alignment(-1, 0)),
-                ),
-                SizedBox(
-                  width: MediaQuery
-                      .of(context)
-                      .size
-                      .width * 0.3,
-                  child: Align(
-                      child: Text(leaderboard[i].points.toString()),
-                      alignment: Alignment(-1, 0)),
-                ),
-                Text(leaderboard[i].gift != null ? leaderboard[i].gift : "--"),
-              ],
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.3,
+                    child: Align(
+                        child: Text(leaderboard[i].name),
+                        alignment: Alignment(-1, 0)),
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.22,
+                    child: Align(
+                        child: CellText(
+                          color: Colors.amber,
+                          text: leaderboard[i].points.toString(),
+                          icon: Icons.local_parking,
+                        ),
+                        alignment: Alignment(-1, 0)),
+                  ),
+                  CellText(
+                    color: Colors.lightGreen,
+                    text: leaderboard[i].gift != null
+                        ? leaderboard[i].gift
+                        : '--',
+                    icon: Icons.monetization_on,
+                  ),
+                ],
+              ),
             ),
           );
         });
@@ -285,20 +304,20 @@ class CellText extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
         child: Row(
-          children: <Widget>[
-            Text(
-              text,
-              style: TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.bold,
-                  fontStyle: FontStyle.italic),
-            ),
-            Icon(
-              icon,
-              color: color,
-              size: 15,
-            )
-          ],
-        ));
+      children: <Widget>[
+        Text(
+          text,
+          style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.bold,
+              fontStyle: FontStyle.italic),
+        ),
+        Icon(
+          icon,
+          color: color,
+          size: 15,
+        )
+      ],
+    ));
   }
 }
